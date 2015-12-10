204104b1745001bd2ba9bd080dcb8bada62f7ed5
Axel Friberg <mail@axelfriberg.com>
2015-12-10 12:00:31 +0100

    Implemented letter.

--- Parser.hs
+++ Parser.hs
@@ -13,6 +13,7 @@ type T a = Parser a
 err :: String -> Parser a
 err message cs = error (message++" near "++cs++"\n")
 
+-- Iterates a parser as long as it succeeds.
 iter :: Parser a -> Parser [a]  
 iter m = m # iter m >-> cons ! return [] 
 
@@ -31,11 +32,9 @@ token :: Parser a -> Parser a
 token m = m #- spaces
 
 -- letter is a parser for a letter as defined by the Prelude function isAlpha (True if the character is an alphabetic character).
-{- l1 = letter "abc"   {- Just('a',"bc") -}
-l2 = letter "123"   {- Nothing -}
-l3 = letter ""      {- Nothing -} -}
+-- Applies the char function to the string and checks if it is alphabetic character uing the prelud function.
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter =  (char ? isAlpha)
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
