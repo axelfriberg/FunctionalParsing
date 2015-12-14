fa505b5bb88646b132ce4de1ea160e40307b0777
Axel Friberg <mail@axelfriberg.com>
2015-12-14 15:31:56 +0100

    Implemented Program.parse

--- Program.hs
+++ Program.hs
@@ -1,6 +1,10 @@
 {- 
 contains a data type for representing a program, a program parser, a program interpreter, 
 and a function for converting the representation to a string.
+
+In the Program module you should represent the program as a Statement list. 
+Use the parse function from the Statement module to define the parse function in this module. 
+Use the exec function in the Statement module to execute a program.
 -}
 
 module Program(T, parse, fromString, toString, exec) where
@@ -8,9 +12,9 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+newtype T = Program [Statement.T] -- to be defined
 instance Parse T where
-  parse = error "Program.parse not implemented"
+  parse = iter Statement.parse >-> Program
   toString = error "Program.toString not implemented"
              
 exec = error "Program.exec not implemented"
